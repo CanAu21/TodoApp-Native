df89655b12aa7a0e81e54b28635b2c325f4da368
CanAu21 <furkan.adrian9@outlook.com>
2024-02-11 18:19:20 -0800

    initialize project

---
+++ App.jsx
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+
+
+function App() {
+
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Todo App</Text>
+      </View>
+    
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
