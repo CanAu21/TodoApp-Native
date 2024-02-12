cdd4fc229d44687481fb08e3b2c2ab010673fbb9
CanAu21 <furkan.adrian9@outlook.com>
2024-02-11 20:20:17 -0800

    input components created

--- App.jsx
+++ App.jsx
@@ -5,28 +5,75 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Alert,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 import Header from './src/components/header';
 import generalStyles from './src/utils/generalStyles';
+import Input from './src/components/input';
+import {colors} from './src/utils/constant';
 
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  // Add ToDo
+  const addTodo = () => {
+    const newTodo = {
+      id: String(new Date().getTime()),
+      text: text,
+      date: new Date(),
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setText('');
+  };
+
   return (
-    <SafeAreaView style={generalStyles.flex1}>
+    <SafeAreaView style={[generalStyles.flex1, generalStyles.bgWhite]}>
       <Header title="My Todo App" />
-      <View>
-        <Text>Todo App week1</Text>
+      <Input
+        onIconPress={() => Alert.alert('İcona tıkladın')}
+        placeholder="deneme"
+        hasIcon
+        value={text}
+        onChangeText={text => setText(text)}
+        onIconPress={addTodo}
+      />
+      <View style={styles.todosWrapper}>
+        {todos.length === 0 ? (
+          <Text style={styles.emptyText}>ToDo not found</Text>
+        ) : (
+          <ScrollView style={styles.scrollView}>
+            <Text>Todo</Text>
+          </ScrollView>
+        )}
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  todosWrapper: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginVertical: 30,
+  },
+  emptyText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.textPrimary,
+  },
+  scrollView: {
+    flexGrow: 1,
+  },
+});
 
 export default App;
